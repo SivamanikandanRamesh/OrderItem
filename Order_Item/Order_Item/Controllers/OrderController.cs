7387f8d2a6ddae7b0828bb4f4b88dae59f3b6543
Surviva <sivarotary007@gmail.com>
2021-06-16 10:56:08 +0530

    Order

---
+++ Order_Item/Order_Item/Controllers/OrderController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Order_Item.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Order.Controllers
+{
+    [Route("api/order")]
+    [ApiController]
+    public class OrderController : ControllerBase
+    {
+        public List<Cart> cart = new List<Cart>()
+        {
+            new Cart(){Id=1,userId=101,menuItemId=1001,menuItemName="Shawarma"},
+            new Cart(){Id=2,userId=102,menuItemId=1002,menuItemName="Mojito"},
+            new Cart(){Id=3,userId=103,menuItemId=1003,menuItemName="Icecream"}
+        };
+        [HttpGet]
+        [Route("Get")]
+        public IEnumerable<Cart> Get()
+        {
+            return cart;
+        }
+        [HttpGet]
+        [Route("{id}")]
+        public IEnumerable<Cart> Get(int id)
+        {
+            return cart;
+        }
+    }
+}
---
+++ Order_Item/Order_Item/Model/Cart.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Order_Item.Model
+{
+    public class Cart
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public int userId { get; set; }
+        public int menuItemId { get; set; }
+        [Required]
+        public string menuItemName { get; set; }
+    }
+}
